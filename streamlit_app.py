bc4a7b62f6ff42df0155c0898a976d382f72c5cb
madofuller <madofuller@gmail.com>
2023-08-16 13:00:04 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 import pandas as pd
-from sklearn import datasets
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import GradientBoostingRegressor
 
