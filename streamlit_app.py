c5fe75771fad67e7c223be6a6958ca81ee2e6944
madofuller <madofuller@gmail.com>
2023-08-16 14:04:33 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 import pandas as pd
+import sklearn
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error
 from math import sqrt
